fd5566b918c0b00608e187ac0c3df109671bdaa5
tamaki1220 <daiya.swmr@gmail.com>
2025-10-16 22:58:50 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,19 @@
+let num= num>=1
+
+console.log(num);
+
+if(num % 3 == 0){
+  console.log('3の倍数です');
+}
+
+if(num % 5 == 0){
+  console.log('5の倍数です');
+}
+
+if(num % 3*5 == 0){
+  console.log('15の倍数です');
+}
+
+else{
+  console.log('num')
+}
